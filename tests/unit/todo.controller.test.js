571572bfa1799a7aa0cbfc745325bc7eca67019c
NUTSRK <nutsrk@odds.team>
2021-08-17 19:49:13 +0700

    day1 :tada:

---
+++ tests/unit/todo.controller.test.js
@@ -0,0 +1,8 @@
+const TodoController = require("../../controllers/todo.controller");
+
+describe("TodoController.createTodo", () => {
+    it("should have a createTodo function", () => {
+        expect(typeof TodoController.createTodo).toBe("function");
+    });
+});
+
