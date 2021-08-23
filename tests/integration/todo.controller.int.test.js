1c469a79befbc0dd086a26b6f5daba13a1078835
NUTSRK <nutsrk@odds.team>
2021-08-23 19:01:01 +0700

    Day 4 🍅

--- tests/integration/todo.controller.int.test.js
+++ tests/integration/todo.controller.int.test.js
@@ -8,11 +8,21 @@ const endpointUrl = "/todos/";
 
 describe(endpointUrl, () => {
     it("POST" + endpointUrl, async () => {
-       const response = await request(app)
-       .post(endpointUrl)
-       .send(newTodo);
-       expect(response.statusCode).toBe(201);
-       expect(response.body.title).toBe(newTodo.title);
-       expect(response.body.done).toBe(newTodo.done);
+        const response = await request(app)
+            .post(endpointUrl)
+            .send(newTodo);
+        expect(response.statusCode).toBe(201);
+        expect(response.body.title).toBe(newTodo.title);
+        expect(response.body.done).toBe(newTodo.done);
+    });
+
+    it("should return error 500 on malformed data with POST" + endpointUrl, async () => {
+        const response = await request(app)
+            .post(endpointUrl)
+            .send({ title: "Missing done property" })
+        expect(response.statusCode).toBe(500);
+        expect(response.body).toStrictEqual({
+            message: "Todo validation failed: done: Path `done` is required."
+        })
     })
 })
